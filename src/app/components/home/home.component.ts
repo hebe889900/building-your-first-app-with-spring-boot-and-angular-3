6610c7eba4ed7fc32fb1d3412fea2dee4a6de2d4
Yan <jackyan63@gmail.com>
2018-06-12 12:20:45 -0400

    added home component form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { BikeService } from '../../service/bike.service'
+import { BikeService } from '../../service/bike.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  models: string[] = [
+    'Globo MTB 29 Full Suspension',
+    'Globo Carbon Fiber Race Series',
+    'Globo Time Trial Blade'
+  ];
+  bikeform: FormGroup;
+  validMessage: string = "";
   constructor(private bikeService:BikeService) { }
 
   ngOnInit() {
+    this.bikeform = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required), 
+      model: new FormControl('', Validators.required),
+      serialNumber: new FormControl('', Validators.required),
+      purchasePrice: new FormControl('', Validators.required),
+      purchaseDate: new FormControl('', Validators.required),
+      contact: new FormControl()
+    })
   }
 
 }
